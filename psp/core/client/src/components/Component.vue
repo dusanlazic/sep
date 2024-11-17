406b5a732442eb4c610a2e9816b7e6c6f66ca1db
Milan Sekulic <sekulic.milan@protonmail.com>
2024-11-17 02:41:21 +0100

    chore: initialise vue+tailwind clients

---
+++ psp/core/client/src/components/Component.vue
@@ -0,0 +1,3 @@
+<template>
+  <p class="text-xl text-emerald-500">Hi</p>
+</template>
---
+++ psp/core/client/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Component from '../components/Component.vue'
+</script>
+
+<template>
+  <div>
+    <Component />
+  </div>
+</template>
