fbb46819af725f66af8b049ad6f739c5a8733a94
Milan Sekulic <sekulic.milan@protonmail.com>
2024-11-25 20:15:48 +0100

    feat: add psp landing page

--- psp/core/frontend/src/router/index.js
+++ psp/core/frontend/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import NotFoundView from '../views/NotFoundView.vue';
-import PaymentView from '../views/PaymentView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import PaymentView from '@/views/PaymentView.vue';
+import LandingView from '@/views/LandingView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: LandingView,
+    },
     {
       path: '/payment',
       name: 'payment',
