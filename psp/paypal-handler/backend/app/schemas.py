99d987cb8a6ea4151834ed5c725edb73b27aa001
xismiq <164878984+xismiq@users.noreply.github.com>
2025-07-18 17:14:46 +0200

    feat: implement paypal handler
    * feat: implement paypal handler
    
    * chore: fill missing data flow
    
    * feat: implement custom payee orders

--- psp/paypal-handler/backend/app/schemas.py
+++ psp/paypal-handler/backend/app/schemas.py
@@ -1,4 +1,10 @@
-from pydantic import BaseModel
+from pydantic import UUID4, BaseModel, HttpUrl, PositiveFloat
+
+
+class NextUrls(BaseModel):
+    success: HttpUrl
+    failure: HttpUrl
+    error: HttpUrl
 
 
 class FooRequest(BaseModel):
@@ -7,3 +13,34 @@ class FooRequest(BaseModel):
 
 class BarResponse(BaseModel):
     buzz: int
+
+
+class TransactionProceedRequest(BaseModel):
+    id: UUID4
+    merchant_id: UUID4
+    amount: PositiveFloat
+    next_urls: NextUrls
+
+
+class TransactionProceedResponse(BaseModel):
+    payment_url: HttpUrl
+
+
+class TransactionDetailsResponse(BaseModel):
+    deposit_address: str
+    amount: PositiveFloat
+    urls: NextUrls
+    status: str
+
+
+class MerchantConfiguration(BaseModel):
+    paypal_merchant_email: str
+
+
+class ConfigureMerchantRequest(BaseModel):
+    merchant_id: UUID4
+    configuration: MerchantConfiguration
+
+class HandlerConfigurationSchemaResponse(BaseModel):
+    title: str
+    configuration_schema: dict
